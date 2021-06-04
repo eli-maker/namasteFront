e2b0c577b3c2535d2b4fd70be5138106ae595c53
Eliana Moreno <eliana.morenoestrada@gmail.com>
2021-06-04 14:03:55 -0400

    popUps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { routing } from './app.routing'
 import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MarkerService } from './service/marker.service';
+import { PopupService } from './popup.service';
 
 
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { MapComponent } from './components/map/map.component';
     FormsModule, HttpClientModule
   ],
   providers: [
-    MarkerService
+    MarkerService,
+    PopupService
   ],
   bootstrap: [AppComponent]
 })
